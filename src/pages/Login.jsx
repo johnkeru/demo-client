02145609423b17f88566d05d2f24b5a35fa5e728
john keru <keruk355@gmail.com>
2024-09-18 14:41:37 +0800

    package.json updated

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -12,10 +12,10 @@ const Login = () => {
     } = useForm()
 
     const onSubmit = (data) => {
-        console.log(data)
+        // console.log(data)
     }
 
-
+    console.log(errors)
     return (
         <Box sx={{ width: 300, m: 'auto', mt: 10 }}>
             <Typography variant='h4' mb={2}>
@@ -23,19 +23,23 @@ const Login = () => {
             </Typography>
             <form onSubmit={handleSubmit(onSubmit)}>
                 <TextField
+                    error={errors?.username?.message ? true : false}
                     id="outlined-text"
                     label="Username"
                     placeholder='Enter Username'
                     fullWidth
-                    {...register('username')}
+                    {...register('username', { required: 'Username is required', })}
                 />
+                <Typography color='error'>{errors?.username?.message}</Typography>
                 <TextField
+                    error={errors?.password?.message ? true : false}
                     id="outlined-text"
                     label="Password"
                     placeholder='**********'
                     fullWidth
-                    {...register('password')}
+                    {...register('password', { required: 'Password is required', })}
                 />
+                <Typography color='error'>{errors?.password?.message}</Typography>
                 <Button type="submit" sx={{ mt: 5 }} variant='contained' fullWidth>Submit</Button>
             </form>
             <Link to='/register'>Don't have an account?</Link>
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,28 +1,49 @@
-import { Box, Button, TextField, Typography } from '@mui/material'
+import { Box, Typography, Button, TextField } from '@mui/material'
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { useForm } from 'react-hook-form'
 
 const Register = () => {
-    return (<Box sx={{ width: 300, m: 'auto', mt: 10 }}>
-        <Typography variant='h4' mb={2}>
-            Sign Up
-        </Typography>
 
-        <TextField
-            id="outlined-text"
-            label="Username"
-            placeholder='Enter Username'
-            fullWidth
-        />
-        <TextField
-            id="outlined-text"
-            label="Password"
-            placeholder='**********'
-            fullWidth
-        />
-        <Button sx={{ mt: 5 }} variant='contained' fullWidth>Submit</Button>
-        <Link to='/login'>Already have an account?</Link>
-    </Box>
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm()
+
+    const onSubmit = (data) => {
+        // console.log(data)
+    }
+
+    console.log(errors)
+    return (
+        <Box sx={{ width: 300, m: 'auto', mt: 10 }}>
+            <Typography variant='h4' mb={2}>
+                Sign In
+            </Typography>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <TextField
+                    error={errors?.username?.message ? true : false}
+                    id="outlined-text"
+                    label="Username"
+                    placeholder='Enter Username'
+                    fullWidth
+                    {...register('username', { required: 'Username is required', })}
+                />
+                <Typography color='error'>{errors?.username?.message}</Typography>
+                <TextField
+                    error={errors?.password?.message ? true : false}
+                    id="outlined-text"
+                    label="Password"
+                    placeholder='**********'
+                    fullWidth
+                    {...register('password', { required: 'Password is required', })}
+                />
+                <Typography color='error'>{errors?.password?.message}</Typography>
+                <Button type="submit" sx={{ mt: 5 }} variant='contained' fullWidth>Submit</Button>
+            </form>
+            <Link to='/login'>Already have an account?</Link>
+        </Box>
     )
 }
 
