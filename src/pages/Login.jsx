d4212c0a03f50f8566e79b3aaf7201804a9209c2
john keru <keruk355@gmail.com>
2024-09-18 14:11:54 +0800

    package.json updated

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,27 +1,43 @@
 import { Box, Typography, Button, TextField } from '@mui/material'
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { useForm } from 'react-hook-form'
 
 const Login = () => {
+
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm()
+
+    const onSubmit = (data) => {
+        console.log(data)
+    }
+
+
     return (
         <Box sx={{ width: 300, m: 'auto', mt: 10 }}>
             <Typography variant='h4' mb={2}>
                 Sign In
             </Typography>
-
-            <TextField
-                id="outlined-text"
-                label="Username"
-                placeholder='Enter Username'
-                fullWidth
-            />
-            <TextField
-                id="outlined-text"
-                label="Password"
-                placeholder='**********'
-                fullWidth
-            />
-            <Button sx={{ mt: 5 }} variant='contained' fullWidth>Submit</Button>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <TextField
+                    id="outlined-text"
+                    label="Username"
+                    placeholder='Enter Username'
+                    fullWidth
+                    {...register('username')}
+                />
+                <TextField
+                    id="outlined-text"
+                    label="Password"
+                    placeholder='**********'
+                    fullWidth
+                    {...register('password')}
+                />
+                <Button type="submit" sx={{ mt: 5 }} variant='contained' fullWidth>Submit</Button>
+            </form>
             <Link to='/register'>Don't have an account?</Link>
         </Box>
     )
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,8 +1,28 @@
+import { Box, Button, TextField, Typography } from '@mui/material'
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Register = () => {
-    return (
-        <div>Register</div>
+    return (<Box sx={{ width: 300, m: 'auto', mt: 10 }}>
+        <Typography variant='h4' mb={2}>
+            Sign Up
+        </Typography>
+
+        <TextField
+            id="outlined-text"
+            label="Username"
+            placeholder='Enter Username'
+            fullWidth
+        />
+        <TextField
+            id="outlined-text"
+            label="Password"
+            placeholder='**********'
+            fullWidth
+        />
+        <Button sx={{ mt: 5 }} variant='contained' fullWidth>Submit</Button>
+        <Link to='/login'>Already have an account?</Link>
+    </Box>
     )
 }
 
