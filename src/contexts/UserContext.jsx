f4510f92004b031c3cb418963eb4860f047b8e2c
john keru <keruk355@gmail.com>
2024-09-19 10:48:04 +0800

    iddleware auth added

--- src/contexts/UserContext.jsx
+++ src/contexts/UserContext.jsx
@@ -6,15 +6,16 @@ const UserContext = createContext()
 
 const UserProvider = ({ children }) => {
     const [user, setUser] = useState(null)
+    const [token, setToken] = useState('')
     const nav = useNavigate();
 
     useEffect(() => {
-        api.get('/currentUser')
+        api.get('/currentUser', { headers: { Authorization: `Bearer ${token}` } })
             .then(res => setUser(res.data.user))
             .catch(() => nav('/login'))
-    }, [])
+    }, [token])
 
-    return <UserContext.Provider value={{ user }}>
+    return <UserContext.Provider value={{ user, setToken }}>
         {children}
     </UserContext.Provider>
 }
