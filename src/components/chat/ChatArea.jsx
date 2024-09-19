b5394d1e59273fa95b6242e8bfe6fc791230427c
john keru <keruk355@gmail.com>
2024-09-19 13:39:17 +0800

    iddleware auth added

--- src/components/chat/ChatArea.jsx
+++ src/components/chat/ChatArea.jsx
@@ -1,10 +1,22 @@
-import { Box } from '@mui/material'
+import { Box, Button, TextField, Typography } from '@mui/material'
 import React from 'react'
 
 const ChatArea = ({ selectedUser }) => {
     return (
-        <Box width={'100%'}>
-            You selected: {selectedUser.username}
+        <Box width={'100%'} sx={{ background: 'gray' }}>
+            {/* header */}
+            <Typography variant='h4'>{selectedUser.username}</Typography>
+            {/* chat area */}
+            <Box height='82.5%' sx={{ background: 'lightgray' }}>
+                <Typography variant='body2'>
+                    Hello,
+                </Typography>
+            </Box>
+            {/* input & send button */}
+            <Box display='flex' sx={{ background: 'white' }}>
+                <TextField fullWidth placeholder='Send message...' />
+                <Button variant='contained'>Send</Button>
+            </Box>
         </Box>
     )
 }
